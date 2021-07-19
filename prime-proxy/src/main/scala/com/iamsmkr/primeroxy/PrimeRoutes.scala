3c74ff36323e7aeb70d0a02fed5c70a6911a05e9
iamsmkr <mailtoshivamk@gmail.com>
2021-07-19 20:03:57 +0000

    number validation

--- prime-proxy/src/main/scala/com/iamsmkr/primeroxy/PrimeRoutes.scala
+++ prime-proxy/src/main/scala/com/iamsmkr/primeroxy/PrimeRoutes.scala
@@ -9,6 +9,7 @@ import akka.stream.scaladsl._
 
 import scala.concurrent._
 import akka.http.scaladsl.marshalling.sse.EventStreamMarshalling._
+import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.sse.ServerSentEvent
 import akka.http.scaladsl.server.Route
 
@@ -17,36 +18,50 @@ import scala.util.{Failure, Success}
 
 class PrimeRoutes(log: LoggingAdapter, client: PrimeGeneratorServiceClient)(implicit mat: Materializer, ec: ExecutionContext) {
 
+  import PrimeRoutes._
+
   private val MAX_ALLOWED_SIZE = 10000
 
   val routes: Route =
     path("prime" / LongNumber) { number =>
       get {
-        log.info("prime numbers request")
+        log.info(s"prime numbers up until number $number are requested")
 
-        val f = client.getPrimeNumbers(GetPrimeNumbersRequest(number))
-          .map(_.primeNumber)
-          .take(MAX_ALLOWED_SIZE)
-          .runWith(Sink.seq)
-          .map(_.mkString(","))
+        val res = validateNumber(number)
 
-        onComplete(f) {
-          case Success(reply) => complete(reply)
-          case Failure(t) =>
-            log.error(t, "Request failed")
-            complete(StatusCodes.InternalServerError, t.getMessage)
+        if (res.isDefined) complete(BadRequest, res.get.msg)
+        else {
+          val f = client.getPrimeNumbers(GetPrimeNumbersRequest(number))
+            .map(_.primeNumber)
+            .take(MAX_ALLOWED_SIZE)
+            .runWith(Sink.seq)
+            .map(_.mkString(","))
+
+          onComplete(f) {
+            case Success(reply) => complete(reply)
+            case Failure(t) =>
+              log.error(t, "Request failed")
+              complete(StatusCodes.InternalServerError, t.getMessage)
+          }
         }
+
       }
     } ~ path("prime" / LongNumber / "sse") { number =>
       get {
-        log.info("prime numbers sse request")
+        log.info(s"prime numbers up until number $number are requested as sse")
 
-        complete {
-          client.getPrimeNumbers(GetPrimeNumbersRequest(number))
-            .map(_.primeNumber)
-            .map(n => ServerSentEvent(n.toString))
-            .keepAlive(1.second, () => ServerSentEvent.heartbeat)
+        val res = validateNumber(number)
+
+        if (res.isDefined) complete(BadRequest, res.get.msg)
+        else {
+          complete {
+            client.getPrimeNumbers(GetPrimeNumbersRequest(number))
+              .map(_.primeNumber)
+              .map(n => ServerSentEvent(n.toString))
+              .keepAlive(1.second, () => ServerSentEvent.heartbeat)
+          }
         }
+
       }
     }
 }
@@ -54,4 +69,11 @@ class PrimeRoutes(log: LoggingAdapter, client: PrimeGeneratorServiceClient)(impl
 object PrimeRoutes {
   def apply(log: LoggingAdapter, client: PrimeGeneratorServiceClient)(implicit mat: Materializer, ec: ExecutionContext): PrimeRoutes =
     new PrimeRoutes(log, client)(mat, ec)
+
+  case class InvalidNumber(msg: String)
+
+  def validateNumber(number: Long): Option[InvalidNumber] = {
+    if (number < 2) Some(InvalidNumber("Please provide a number greater 1"))
+    else None
+  }
 }
